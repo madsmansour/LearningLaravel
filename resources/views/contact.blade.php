c24d55b224a2db2de01dcc5aa0e7c62060a8b719
Mads Mansour Larsen <42993941+madsmansour@users.noreply.github.com>
2019-12-21 11:29:50 +0100

    Contacts added

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,20 +1,13 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact</title>
-</head>
-<body>
-<h1>Contact information</h1>
+@extends('layout')
 
-<ul>
-    <?php foreach ($contacts as $contact) : ?>
-    <strong><li><?= $contact; ?></li></strong><br>
-    <?php endforeach; ?>
-</ul>
-<a href="/">Home</a>
-</body>
-</html>
+@section('content')
+    <h1>My first website</h1>
+    <ul>
+        <?php foreach ($contacts as $contact) : ?>
+        <strong><li><?= $contact; ?></li></strong><br>
+        <?php endforeach; ?>
+    </ul>
+
+@endsection
+
+
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+ <!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+</head>
+<body>
+@yield('content')
+
+<a href="/">Home</a>
+<br>
+<a href="/contact">Contacts</a>
+
+</body>
+</html>
